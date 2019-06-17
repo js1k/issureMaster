cdbc8e33793c46dd5380788892cf0a9b2f9c77b6
WWE <oncwnuNlMZz6ztCBJdwxFbwbej-U@git.weixin.qq.com>
2019-06-17 20:13:47 +0800

    开发答题页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,110 +3,113 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    showRules:false,
-    showCardImg:'../../asset/index/xinren.png',
-    showCardName:'帮帮卡',
-    knapsackMask:false,
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  openRules:function(){
-    this.setData({
-      showRules:true
-    })
-  },
-  closeRules: function (event){
-    if (event.currentTarget.dataset.model==='inner'){
-      return
-    }
-    this.setData({
-      showRules:false
-    })
-  },
-  goTreasure:function(){
-    wx.navigateTo({
-      url: '../treasureBox/treasureBox'
-    })
-  },
-  goRank: function () {
-    wx.navigateTo({
-      url: '../rankList/rankList'
-    })
-  },
-  goRedPackets: function () {
-    wx.navigateTo({
-      url: '../redPackets/redPackets'
-    })
-  },
-  showCard: function (event){
-    this.setData({
-      showCardImg: event.currentTarget.dataset.src,
-      showCardName: event.currentTarget.dataset.name
-    })
-  },
-  showKnapsack: function () {
-    this.setData({
-      knapsackMask: true
-    })
-  },
-  closeMask: function (event) {
-    if (event.currentTarget.dataset.model === 'inner') {
-      return
-    }
-    this.setData({
-      knapsackMask:false
-    })
-  },
-  goHomepage: function () {
-    wx.navigateTo({
-      url: '../homepage/homepage'
-    })
-  },
-  onLoad: function () {
-
-
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        showRules: false,
+        showCardImg: '../../asset/index/xinren.png',
+        showCardName: '帮帮卡',
+        knapsackMask: false,
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo')
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.navigateTo({
+            url: '../logs/logs'
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    },
+    openRules: function() {
+        this.setData({
+            showRules: true
+        })
+    },
+    closeRules: function(event) {
+        if (event.currentTarget.dataset.model === 'inner') {
+            return
         }
-      })
+        this.setData({
+            showRules: false
+        })
+    },
+    goTreasure: function() {
+        wx.navigateTo({
+            url: '../treasureBox/treasureBox'
+        })
+    },
+    goRank: function() {
+        wx.navigateTo({
+            url: '../rankList/rankList'
+        })
+    },
+    goRedPackets: function() {
+        wx.navigateTo({
+            url: '../redPackets/redPackets'
+        })
+    },
+    goChallenge: function () {
+        wx.navigateTo({
+            url: '../challengeHome/challengeHome'
+        })
+    },
+    showCard: function(event) {
+        this.setData({
+            showCardImg: event.currentTarget.dataset.src,
+            showCardName: event.currentTarget.dataset.name
+        })
+    },
+    showKnapsack: function() {
+        this.setData({
+            knapsackMask: true
+        })
+    },
+    closeMask: function(event) {
+        if (event.currentTarget.dataset.model === 'inner') {
+            return
+        }
+        this.setData({
+            knapsackMask: false
+        })
+    },
+    goHomepage: function() {
+        wx.navigateTo({
+            url: '../homepage/homepage'
+        })
+    },
+    onLoad: function() {
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
+    },
+    getUserInfo: function(e) {
+        console.log(e)
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
