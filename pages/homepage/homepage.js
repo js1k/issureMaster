b02182d435c608567dda265df3f0cf9015bc03cb
WWE <oncwnuNlMZz6ztCBJdwxFbwbej-U@git.weixin.qq.com>
2019-06-24 14:59:56 +0800

    对接个人主页

--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -2,69 +2,90 @@
 const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        insureUserVO:'',
+        seasonData:[],
+        darenImg: '../../asset/rankList/daren_pic.png',
+        gaoshouImg: '../../asset/rankList/gaoshou_pic.png',
+        dashiImg: '../../asset/rankList/dashi_pic.png',
+        zongshiImg: '../../asset/rankList/zongshi_pic.png',
+        darenIcon: '../../asset/rankList/icon_daren.png',
+        gaoshouIcon: '../../asset/rankList/icon_gaoshou.png',
+        dashiIcon: '../../asset/rankList/icon_dashi.png',
+        zongshiIcon: '../../asset/rankList/icon_zongshi.png',
+    },
 
-  },
+    goBack: function() {
+        app.goBack()
+    },
+    getData: function() {
+        let _this = this
+        app.httpPost('/xcx/insureMaster/personalCenter', { insureUid: app.globalData.insureUid}, function(data) {
+            console.log(data)
+            _this.setData({
+                insureUserVO: data.insureUserVO,
+                seasonData: data.recordVOList
+            })
+        }, function(data) {
+            console.log('error')
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.getData()
+    },
 
-  goBack: function () {
-    app.goBack()
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    }
 })
