48cf2daac8bdd9ae6d93804559dd8b42a88a93f3
WWE <oncwnuNlMZz6ztCBJdwxFbwbej-U@git.weixin.qq.com>
2019-07-03 17:28:02 +0800

    challenge page function dev

--- pages/challenge/challenge.js
+++ pages/challenge/challenge.js
@@ -6,10 +6,17 @@ Page({
      */
     data: {
         curIndex: 0,    //当前答题序号
-        countDown: 60,
+        showMask:false,
+        countDownTime: 60,
         unfinshed: false,
         interval: null,
         timeInterval: null,
+        showShare:false,
+        answerEnd:true,
+        hiddenLoading:true,
+        showShareActive:false,
+        creatImg:true,
+        loadingText:'图片生成中...',
         examUserId: wx.getStorageSync('question').examUserId,
         question: wx.getStorageSync('question').subjectList,
         bangbang: '../../asset/challengeHome/bangbang_icon.png',
@@ -20,10 +27,11 @@ Page({
         removeCard: wx.getStorageSync('removeCard'),
         curQuestion: '',
         choseItem:-1,
+        canSubmit:true,
         subParam:{
             answerList:[],
-            examUserId:'',
-            insureUid:'',
+            examUserId: wx.getStorageSync('question').examUserId,
+            insureUid: wx.getStorageSync('insureUid'),
             subjectIdList:[]
         },
         queList: [{
@@ -38,16 +46,26 @@ Page({
                 serial: 'C',
                 question: ''
             }
-        ]
+        ],
+
+        shareCoverImg: 'https://msbxgw.oss-cn-hzfinance.aliyuncs.com/upload/img/20190703/1562141592564.png',
+        shareQrImg: '',
     },
 
-    onShareAppMessage: function() {
-        return {
+    onShareAppMessage: function (options) {
+        let param = {
             title: '2019民生保险用户体验节~ \n保保大师答题挑战赛，精彩来战',
             path: '/pages/index/index',
             imageUrl: 'https://msbxgw.oss-cn-hzfinance.aliyuncs.com/upload/img/20190628/1561717521552.png',
-            success: function() {}
+            success: function () {
+
+            }
         }
+        if (options.from === 'button') {
+            var dataid = options.target.dataset;
+            param.path = '/pages/index/index'
+        }
+        return { ...param }
     },
     /**
      * 生命周期函数--监听页面加载
@@ -57,10 +75,85 @@ Page({
         this.setQuestion(0)
         this.interval = setTimeout(function() {
             _this.calcTime()
-        }, 2000)
-        // this.getData()
+        }, 2300)
     },
-    //处理数据
+    saveImg: function () {
+        wx.canvasToTempFilePath({
+            canvasId: 'shareCanvas',
+            success: function (res) {
+                wx.saveImageToPhotosAlbum({
+                    filePath: res.tempFilePath,
+                    success: function (data) {
+                        wx.showToast({
+                            title: '分享图片已保存到相册',
+                        })
+                    }
+                })
+            }
+        }, this)
+    },
+    handleShare: function () {
+        let _this = this
+        this.setData({
+            hiddenLoading: false
+        })
+        const ctx = wx.createCanvasContext('shareCanvas', this)
+        ctx.setFillStyle('#fff')
+        ctx.fillRect(0, 0, 275, 380)
+        ctx.draw()
+        //获取小程序二维码
+        app.httpPost('/xcx/insureMaster/lookXcxQrCode', {
+            insureUid: wx.getStorageSync('insureUid')
+        }, function (data) {
+            _this.setData({
+                shareQrImg: data.xcxQrCode
+            })
+            wx.getImageInfo({
+                src: _this.data.shareCoverImg,
+                success: (res1) => {
+                    let url = res1.path
+                    ctx.drawImage(res1.path, 0, 0, 275, 300)
+                    wx.getImageInfo({
+                        src: data.xcxQrCode,
+                        success: (res2) => {
+                            let qrImgSize = 70
+                            ctx.drawImage(res2.path, 190, 306, qrImgSize, qrImgSize)
+                            ctx.stroke()
+                            ctx.draw(true)
+                            ctx.setFontSize(14)
+                            ctx.setFillStyle('#000')
+                            ctx.fillText(data.nickName + '送你"分享宝箱"', 16, 330)
+                            ctx.draw(true)
+                            ctx.setFontSize(12)
+                            ctx.setFillStyle('#000')
+                            ctx.fillText('快来和TA一起赢取大奖吧~', 16, 356)
+                            ctx.draw(true)
+                            _this.setData({
+                                creatImg: false,
+                                hiddenLoading: true
+                            })
+                        }
+                    })
+                }
+            })
+
+            _this.setData({
+                showMask: true,
+                showShareActive: true
+            })
+        })
+    },
+    goChallengeHome: function () {
+        wx.navigateTo({
+            url: '../challengeHome/challengeHome'
+        })
+    },
+    goRankList: function () {
+        wx.navigateTo({
+            url: '../rankList/rankList'
+        })
+    },
+    //处理考题
     setQuestion: function (num) {
         if (num>4){
             return
@@ -75,28 +168,60 @@ Page({
             [queListB]: _this.data.question[num].optionB,
             [queListC]: _this.data.question[num].optionC,
         })
+        if (num===0){
+            return 
+        }
+        this.setData({
+            curIndex: _this.data.curIndex + 1,
+            choseItem: -1
+        })
     },
     //选择答案
     choseAnswer:function(e){
         let serial = e.currentTarget.dataset.serial
         let _this = this
-        if (this.data.subParam.answerList.length==5){
+        //  如果五题答完 或者 时间结束 不可选
+        if (this.data.subParam.answerList.length == 5 || this.data.countDownTime==0){
             return
         }
         let idList = 'subParam.subjectIdList[' + this.data.subParam.subjectIdList.length + ']'
         let answerList = 'subParam.answerList[' + this.data.subParam.subjectIdList.length + ']'
         _this.setData({
-            curIndex: _this.data.curIndex + 1,
             choseItem: serial == 'A' ? 0 : serial == 'B'?1:2,
             [idList]: _this.data.question[_this.data.curIndex].id,
             [answerList]: serial
         })
-        console.log(this.data.subParam.answerList)
-        this.setQuestion(_this.data.curIndex)
+        setTimeout(function () {
+            _this.setQuestion(_this.data.curIndex+1)
+        },800)
+    },
+    //提交答案
+    submitTest:function(){
+        let _this = this
+        if (!_this.data.canSubmit) {
+            return
+        }
+        _this.setData({
+            canSubmit: false
+        })
+        app.httpPost('/xcx/insureMaster/examHandIn', _this.data.subParam,function(){
+            wx.showToast({
+                title: '提交成功',
+                icon: 'success',
+                duration: 1000,
+                mask: true
+            })
+            _this.setData({
+                canSubmit:true
+            })
+        },function(error){
+            console.log('error')
+        })
     },
     goBack: function() {
-        if (this.data.countDown > 0) {
+        if (this.data.countDownTime > 0) {
             this.setData({
+                showMask:true,
                 unfinshed: true
             })
             return
@@ -110,16 +235,6 @@ Page({
     cancleBack: function() {
         this.setData({
             unfinshed: false
-        })
-    },
-    getData: function() {
-        app.httpPost('/xcx/insureMaster/examStart', {
-            insureUid: wx.getStorageSync('insureUid'),
-            isUseEnergyCard: 0
-        }, function(data) {
-            console.log(data)
-        }, function(error) {
-            console.log('error')
         })
     },
     handleBang: function() {
@@ -138,12 +253,16 @@ Page({
     calcTime: function() {
         var _this = this
         this.timeInterval = setInterval(function() {
-            if (_this.data.countDown > 0) {
+            if (_this.data.countDownTime > 0) {
                 _this.setData({
-                    countDown: _this.data.countDown - 1
+                    countDownTime: _this.data.countDownTime - 1
                 })
-            } else {
-
+            } else {// 时间结束 自动提交答案
+                _this.setData({
+                    answerEnd:true
+                })
+                _this.submitTest()
+                clearInterval(_this.timeInterval)
             }
         }, 1000)
     },
@@ -196,11 +315,4 @@ Page({
     onReachBottom: function() {
 
     },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
-    }
 })
